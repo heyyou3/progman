a9ea9c5166439579a59d846a476c2f4926eab823
heyyou <nishidayuuhei@gmail.com>
2019-10-30 07:55:04 +0900

    feat: 右下のステータスバーに進捗率を表示させるように実装

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate({ subscriptions }: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -13,14 +13,27 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let progManStatusBarItem: vscode.StatusBarItem;
+	progManStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	subscriptions.push(progManStatusBarItem);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	let activeEditor = vscode.window.activeTextEditor;
 
-	context.subscriptions.push(disposable);
+	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+	}));
+
+	subscriptions.push(vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			const activeText = event.document.getText();
+			const todoCount = activeText.split('- [ ] ').length - 1;
+			const doneCount = activeText.split('- [x] ').length - 1;
+			const allTodoCount = todoCount + doneCount;
+			const progress = Math.floor(doneCount / allTodoCount * 100);
+			progManStatusBarItem.text = `☆今の進捗率は${progress}%だよ！やったね☆`;
+			progManStatusBarItem.show();
+		}
+	}));
 }
 
 // this method is called when your extension is deactivated
